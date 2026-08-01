cc92a76b1e255a9638a4201129b9ffaf5c3d9481
Cozma Rares <cozmarares11@gmail.com>
2026-08-02 01:03:01 +0300

    Revert "add last clicked indicator"
    This reverts commit 0e05f3f5555687522508cf40accde61ac21a2796.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,19 +3,50 @@ import type { Category, MarkWithSeries } from "@/db/dal";
 import { serverError, type Page, type PageLoadError } from "./types";
 import { ResultAsync } from "neverthrow";
 
+type AgedMark = MarkWithSeries & { ageIndicatorColor: string };
+
 type Props = {
-    categorizedMarks: Array<Category & { marks: MarkWithSeries[] }>;
-    uncategorizedMarks: MarkWithSeries[];
+    categorizedMarks: Array<Category & { marks: AgedMark[] }>;
+    uncategorizedMarks: AgedMark[];
 };
 
 function dataLoader(): ResultAsync<Props, PageLoadError> {
+    const MS_PER_DAY = 24 * 60 * 60 * 1000;
+
+    const THRESHOLDS = {
+        FRESH: [7, "#22C55E"],
+        AGING: [30, "#EAB308"],
+        STALE: [60, "#F97316"],
+        VERY_STALE: [90, "#EF4444"],
+        ANCIENT: [Infinity, "#6B7280"],
+    } as const;
+
+    const now = Date.now();
+
+    const createAgedMark = (mark: MarkWithSeries) => {
+        const days = (now - mark.updatedAt.getTime()) / MS_PER_DAY;
+
+        const color = Object.values(THRESHOLDS).find(
+            ([threshold]) => {
+                if (isNaN(days)) {
+                    console.log(mark)
+                }
+                return days < threshold;
+            }
+        )![1];
+        return { ...mark, ageIndicatorColor: color };
+    };
+
     return ResultAsync.combineWithAllErrors([
         getCategorizedMarks(),
         getUncategorizedMarks(),
     ])
         .map(([categorizedMarks, uncategorizedMarks]) => ({
-            categorizedMarks,
-            uncategorizedMarks,
+            categorizedMarks: categorizedMarks.map(category => ({
+                ...category,
+                marks: category.marks.map(createAgedMark),
+            })),
+            uncategorizedMarks: uncategorizedMarks.map(createAgedMark),
         }))
         .mapErr(serverError);
 }
@@ -37,9 +68,9 @@ function component({ categorizedMarks, uncategorizedMarks }: Props) {
                     </div>
 
                     <ul class="home-list">
-                        {uncategorizedMarks.map(mark => (
+                        {uncategorizedMarks.map(agedMark => (
                             <li class="home-list-item">
-                                <MarkComponent mark={mark} />
+                                <MarkComponent agedMark={agedMark} />
                             </li>
                         ))}
                     </ul>
@@ -66,9 +97,9 @@ function component({ categorizedMarks, uncategorizedMarks }: Props) {
                     </div>
                     {category.marks.length > 0 ? (
                         <ul class="home-list">
-                            {category.marks.map(mark => (
+                            {category.marks.map(agedMark => (
                                 <li class="home-list-item">
-                                    <MarkComponent mark={mark} />
+                                    <MarkComponent agedMark={agedMark} />
                                 </li>
                             ))}
                         </ul>
@@ -93,11 +124,11 @@ function component({ categorizedMarks, uncategorizedMarks }: Props) {
 }
 
 type MarkProps = {
-    mark: MarkWithSeries;
+    agedMark: AgedMark;
 };
 
-function MarkComponent({ mark }: MarkProps) {
-    const { series } = mark;
+function MarkComponent({ agedMark }: MarkProps) {
+    const { series, ...mark } = agedMark;
 
     return (
         <div
@@ -106,11 +137,10 @@ function MarkComponent({ mark }: MarkProps) {
             data-mark-url={mark.url}
             data-mark-title={mark.title ?? ""}
             data-mark-category-id={mark.categoryId ?? ""}
-            data-mark-updated-at={mark.updatedAt.getTime()}
         >
             <div
-                class="mark-indicator"
-                data-mark-indicator
+                class="mark-age-indicator"
+                style={`background-color: ${mark.ageIndicatorColor}`}
             />
             {series?.episode && (
                 <span class="mark-episode">[{series.episode}]</span>
